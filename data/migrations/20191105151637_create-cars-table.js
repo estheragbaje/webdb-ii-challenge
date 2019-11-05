868fca50953f23a2b2cbacafb96564b0ebe464ae
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-05 15:47:44 +0100

    Created the database file using knex migrate command

--- data/migrations/20191105151637_create-cars-table.js
+++ data/migrations/20191105151637_create-cars-table.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", table => {
+    table.increments("id");
+    table
+      .text("VIN")
+      .unique()
+      .notNullable();
+    table.text("make").notNullable;
+    table.text("model").notNullable;
+    table.decimal("mileage").notNullable;
+    table.text("transmission");
+    table.text("title");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cars");
 };
