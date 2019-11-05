06260625fb07b9fdbbf1f8acf886033e11daf75f
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-05 23:07:37 +0100

    Strech 2 completed

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -55,4 +55,36 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .update({
+      VIN: req.body.VIN,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage
+    })
+    .then(affectedRecords => {
+      console.log(affectedRecords);
+      res.json(affectedRecords + " car records got changed!");
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "this went wrong: " + error.message
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .del()
+    .then(affectedRecords => {
+      res.json(affectedRecords + " rows got deleted!");
+    })
+    .catch(error => {
+      res.status(500).json("this went wrong: " + error.message);
+    });
+});
+
 module.exports = router;
